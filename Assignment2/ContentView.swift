c235ed73d913183c8e704e56dc2ce5e20d05406b
Jiang Peng Han <jhan111@se12l0325-21.ad.bcit.ca>
2025-02-27 15:42:27 -0800

    initial setup

---
+++ Assignment2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Assignment2
+//
+//  Created by Jiang Peng Han on 2025-02-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
