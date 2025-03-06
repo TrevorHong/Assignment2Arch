1f00a679b08ede3cdae03bedc0a0b394310490c2
Shuang Wu <shuang111994@gmail.com>
2025-03-06 11:29:13 -0800

    attached flashlight to camera again and set it shin forward

--- Assignment2/SceneViewWrapper.swift
+++ Assignment2/SceneViewWrapper.swift
@@ -81,17 +81,18 @@ struct SceneViewWrapper: UIViewRepresentable {
         mazeGenerator.rootNode.addChildNode(cameraNode)
         
         // Add the flashlight and attach it to the camera node
-        addFlashlightToScene(parentNode: mazeGenerator.rootNode)
+        addFlashlightToScene(parentNode: cameraNode)
     }
 
     func addFlashlightToScene(parentNode: SCNNode) {
         let flashlightNode = flashlight.getLightNode()
+        flashlightNode.eulerAngles = SCNVector3(0, 0, 0)
         
         // Add the flashlight as a child of the camera node
         parentNode.addChildNode(flashlightNode)
         
         // Optionally, adjust the flashlight's position relative to the camera
-//        flashlightNode.position = SCNVector3(0, 0, -1)  // Adjust flashlight position in front of the camera
+        flashlightNode.position = SCNVector3(0, 0, 0)  // Adjust flashlight position in front of the camera
     }
 
     func startCubeRotation() {
